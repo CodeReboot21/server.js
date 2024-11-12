754f32aa943f2ca1dd3ad88103af770bb8717045
CodeReboot21 <89827318+CodeReboot21@users.noreply.github.com>
2024-11-12 20:43:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
@@ -14,8 +15,6 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const express = require('express');
-const cors = require('cors');
 
 // Enable CORS to allow requests from other domains
 app.use(cors());
